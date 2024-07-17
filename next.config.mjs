38ccd3cf5c6cf99f6534aab570ab0369dcddcb80
osama-hasan-khan <osamahasankhan098@gmail.com>
2024-07-17 22:21:25 +0530

    fix .env mistake

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import createJiti from "jiti";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
-jiti("./app/env");
+jiti("./lib/env");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
