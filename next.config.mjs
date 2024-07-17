7eb8f6ca1d535686f96251a60ac082ea93ccc3eb
osama-hasan-khan <osamahasankhan098@gmail.com>
2024-07-17 22:29:18 +0530

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 import { fileURLToPath } from "node:url";
+
 import createJiti from "jiti";
+
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
